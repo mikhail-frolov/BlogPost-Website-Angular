dfbf161c301b373d4acbb39dd15889dec6fcbd9b
Mikhail <mishafrolov882000@gmail.com>
2020-12-12 06:12:48 +0300

    Comments Functionality Added

--- my-app/src/app/post-data/post-data.component.ts
+++ my-app/src/app/post-data/post-data.component.ts
@@ -13,16 +13,37 @@ post: BlogPost;
 
 querySub: any;
 
+commentName: string;
+commentText: string;
+
   constructor(private data: PostService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.querySub = this.route.params.subscribe(params =>{
-      this.data.getPostbyId(params['id']).subscribe(data => this.post = data ); 
+      this.data.getPostbyId(params['id']).subscribe(data => {
+        this.post = data,
+       this.post.views++,
+        this.data.updatePostById(this.post._id, this.post).subscribe();
+      }); 
     });
   }
 
   ngOnDestroy() {
-    if(this.querySub) this.querySub.unsubscribe();
+    if(this.querySub)
+    this.querySub.unsubscribe();
+  }
+
+
+  submitComment(): void {
+    this.post.comments.push({ 
+      author: this.commentName,
+      comment: this.commentText,
+      date: new Date().toLocaleDateString()
+    });
+    this.data.updatePostById(this.post._id, this.post).subscribe( () => {
+      this.commentName = '';
+      this.commentText = '';
+    })
   }
 
 }
